c591dac7174eb54bbe6762353beaedf6d9eb97d3
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-03 16:23:23 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by KPS on 7/3/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
